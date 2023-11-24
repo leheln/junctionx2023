825463234f2299dffa332e75da18a151cbaf5dd1
Henrik Élő <henrikeloe@gmail.com>
2023-11-25 00:44:40 +0100

    Add API endpoints

--- backend/src/packages/auth/index.ts
+++ backend/src/packages/auth/index.ts
@@ -2,4 +2,5 @@ export {createFirstUser} from './logic/create-first-user';
 export {loginApi} from './api/login.api';
 export {logoutApi} from './api/logout.api';
 export {whoamiApi} from './api/whoami.api';
+export {userGetApi, userGetConsumptionsApi, userGetEventsApi, userGetPassesApi} from './api/user.api'
 export {authenticationMiddleware} from './middleware/authentication.middleware';
--- backend/src/registry/api.registry.ts
+++ backend/src/registry/api.registry.ts
@@ -1,10 +1,16 @@
 import {Application} from 'express';
-import {authenticationMiddleware as authentication, loginApi, logoutApi, whoamiApi} from '@/packages/auth';
+import {authenticationMiddleware as authentication, loginApi, logoutApi, userGetApi, userGetConsumptionsApi, userGetEventsApi, userGetPassesApi, whoamiApi} from '@/packages/auth';
+
+const API_PREFIX = "/api"
 // Register APIs into this file from each feature
 export const apiRegistry = (app: Application) => {
-    app.post('/api/auth/login', loginApi);
-    app.post('/api/auth/logout', logoutApi);
-    app.post('/api/auth/whoami', whoamiApi);
-
+    app.post(`${API_PREFIX}/auth/login`, loginApi);
+    app.post(`${API_PREFIX}/auth/logout`, logoutApi);
+    app.post(`${API_PREFIX}/auth/whoami`, whoamiApi);
+    
     //app.post('/api/protected/route', authentication, exampleEndpoint);
+    app.get(`${API_PREFIX}/users/:userId`, authentication, userGetApi)
+    app.get(`${API_PREFIX}/users/:userId/events`, authentication, userGetEventsApi)
+    app.get(`${API_PREFIX}/users/:userId/consumptions`, authentication, userGetConsumptionsApi)
+    app.get(`${API_PREFIX}/users/:userId/passes`, authentication, userGetPassesApi)
 };
