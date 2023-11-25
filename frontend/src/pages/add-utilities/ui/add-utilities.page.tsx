3ef5adbfa6e2a63b679f81e57ba1634a3b680eb2
Aron Derenyi <aron.derenyi@gmail.com>
2023-11-25 04:12:07 +0100

    WIP

--- frontend/src/pages/add-utilities/ui/add-utilities.page.tsx
+++ frontend/src/pages/add-utilities/ui/add-utilities.page.tsx
@@ -1,5 +1,5 @@
 import {Layout} from '@/packages/layout';
-import {ChangeEvent, useRef, useState} from "react";
+import {ChangeEvent, FormEventHandler, useRef, useState} from "react";
 import {Button} from "@/components/ui/button.tsx";
 import axios from "axios";
 import SpinLoader from "@/components/spin-loader.tsx";
@@ -13,17 +13,16 @@ interface UtilitiesData {
 }
 
 export function AddUtilitiesPage() {
-    const input = useRef<HTMLInputElement>(null)
     const [loading, setLoading] = useState(false);
     const [data, setData] = useState<UtilitiesData | undefined>(undefined);
 
-    const onFileChangeCapture = (e: ChangeEvent<HTMLInputElement>) => {
+    const onFile = (e: ChangeEvent<HTMLInputElement>) => {
         setLoading(true)
         const file = e.target.files![0]
         const reader = new FileReader()
         reader.readAsDataURL(file)
         reader.onloadend = () => {
-            const data = { image: reader.result }
+            const data = {image: reader.result}
             axios.post('/api/try-utilities', data)
                 .then(response => {
                     setData(response.data)
@@ -41,20 +40,34 @@ export function AddUtilitiesPage() {
 
     return (
         <Layout title="Add utility bill" showBack className="flex flex-col p-4 justify-center items-center gap-4">
-            { data ?
-                <>
-                    type: {data.type}<br/>
-                    type: {data.dateStart}<br/>
-                    type: {data.dateEnd}<br/>
-                    type: {data.amount}
-                </> :
-                <>
-                    <span className="text-xl text-center">Upload an image of your bill</span>
-                    <Button onClick={() => input.current?.click()}>Upload</Button>
-                    <input ref={input} type="file" className="hidden" accept=".png,.jpg,.pdf" onChangeCapture={onFileChangeCapture}></input>
-                </>
-            }
+            {data ? <Accept data={data}/> : <Upload onFile={onFile}/>}
             {loading && <SpinLoader size={32}/>}
         </Layout>
     );
+}
+
+interface UploadProps {
+    onFile: FormEventHandler<HTMLInputElement>
+}
+
+function Upload({onFile}: UploadProps) {
+    const input = useRef<HTMLInputElement>(null)
+    return <>
+        <span className="text-xl text-center">Upload an image of your bill</span>
+        <Button onClick={() => input.current?.click()}>Upload</Button>
+        <input ref={input} type="file" className="hidden" accept=".png,.jpg,.pdf" onChangeCapture={onFile}></input>
+    </>
+}
+
+interface AcceptProps {
+    data: UtilitiesData
+}
+
+function Accept({data}: AcceptProps) {
+    return <>
+        type: {data.type}<br/>
+        start: {data.dateStart}<br/>
+        end: {data.dateEnd}<br/>
+        amount: {data.amount}
+    </>
 }
