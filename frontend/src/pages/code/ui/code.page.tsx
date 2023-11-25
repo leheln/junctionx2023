af2696009a38a8bad65d153dd6fe5d67ca63f398
Lehel Nagy <me@lehelnagy.com>
2023-11-25 15:56:39 +0100

    QR code frontend

--- frontend/src/pages/code/ui/code.page.tsx
+++ frontend/src/pages/code/ui/code.page.tsx
@@ -1,9 +1,41 @@
 import {Layout} from '@/packages/layout';
+import {useEffect, useState} from 'react';
+import {axios} from '@/core/axios';
+import {toast} from '@/components/ui/use-toast.ts';
+import SpinLoader from '@/components/spin-loader.tsx';
 
 export function CodePage() {
+    const [loading, setLoading] = useState(true);
+    const [qrCode, setQrCode] = useState('');
+    useEffect(() => {
+        axios.post('/api/qr/get')
+            .then((response) => {
+                setQrCode(response.data.qr);
+                setLoading(false);
+            })
+            .catch(() => {
+                toast({
+                    variant: 'destructive',
+                    title: 'An error has occurred.',
+                    description: 'Try reloading the page. If the issue persists please contact support.'
+                });
+                setLoading(false);
+            });
+    });
     return (
-        <Layout title="QR Code" showBack>
-            Here is the QR code
+        <Layout title="QR Code" showBack className="flex flex-col items-center p-8 text-center">
+            Show this QR code to event organizers to claim rewards
+            {
+                loading ?
+                    (
+                        <div className="h-[250px] w-[250px] mt-4">
+                            <SpinLoader size={18}/>
+                        </div>
+                    )
+                    : (
+                        <img className="h-[250px] w-[250px] mt-4 invert mix-blend-screen" alt="" src={`data:image/${qrCode}`}/>
+                    )
+            }
         </Layout>
     );
 }
