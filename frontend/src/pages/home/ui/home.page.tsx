536b815bb658403654ea06a9ca7019ce3a087891
Henrik Élő <henrikeloe@gmail.com>
2023-11-25 21:18:26 +0100

    Add credit counter

--- frontend/src/pages/home/ui/home.page.tsx
+++ frontend/src/pages/home/ui/home.page.tsx
@@ -1,10 +1,16 @@
+import { RootState } from '@/core/state';
 import {Layout} from '@/packages/layout';
+import { useSelector } from 'react-redux';
 import {Link} from "react-router-dom";
 
 export function HomePage() {
+    const credits = useSelector((state: RootState) => state.auth.credits)
     return (
         <Layout showNavigation className="flex flex-col">
-            <img style={{ height: "20px", width: "20px" }} src="credit.png" />
+            <div style={{display: "flex", flexFlow: "row", alignItems: "center", gap:'10px'}}> 
+                <img style={{ height: "20px", width: "20px" }} src="credit.png" /> 
+                <span>{credits}</span>
+            </div>
             <Link to="/code">QR Code</Link>
             <Link to="/add-utilities">Add utility bill</Link>
         </Layout>
