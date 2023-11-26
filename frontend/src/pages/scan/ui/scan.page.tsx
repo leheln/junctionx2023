e8f907af3cb672c12ce9b4c4b3fb2e51ad16d8da
Lehel Nagy <me@lehelnagy.com>
2023-11-26 01:43:39 +0100

    Scan page

--- frontend/src/pages/scan/ui/scan.page.tsx
+++ frontend/src/pages/scan/ui/scan.page.tsx
@@ -1,18 +1,35 @@
 import {Layout} from '@/packages/layout';
-import {useState} from 'react';
 import {QrScanner} from '@yudiel/react-qr-scanner';
+import {axios} from '@/core/axios';
+import {toast} from '@/components/ui/use-toast.ts';
+import {useParams} from 'react-router-dom';
 
 export function ScanPage() {
-    const [data, setData] = useState('No result');
+    const { eventId } = useParams();
+    const onDecode = (result: string) => {
+        axios.post(`/events/:${eventId}/users/:${result}/validateParticipation`)
+            .then(() => {
+                toast({
+                    variant: 'default',
+                    title: 'Attendance successfully acknowledged.'
+                });
+            })
+            .catch(() => {
+                toast({
+                    variant: 'destructive',
+                    title: 'An error has occurred.',
+                    description: 'Try reloading the page. If the issue persists please contact support.'
+                });
+            });
+    };
     return (
-        <Layout showBack className="flex flex-col" backgroundImage="/background_green_pale.png">
+        <Layout title="Scan QR Code" showBack className="flex flex-col" backgroundImage="/background_green_pale.png">
             <div className="">
                 <QrScanner
-                    onDecode={(result) => setData(result)}
+                    onDecode={(result) => onDecode(result)}
                     onError={(error) => console.log(error?.message)}
                 />
             </div>
-            <p>{data}</p>
         </Layout>
     );
 }
