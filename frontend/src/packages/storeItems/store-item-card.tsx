87df00daac75d6173feccf8602131b6904000faf
Henrik Élő <henrikeloe@gmail.com>
2023-11-26 03:24:31 +0100

    Change store item list style

--- frontend/src/packages/storeItems/store-item-card.tsx
+++ frontend/src/packages/storeItems/store-item-card.tsx
@@ -1,26 +1,29 @@
 import { StoreItem } from "@/models/storeItem";
-import { useState } from "react";
 import { FaHeart } from "react-icons/fa";
 
 interface StoreItemCardProps {
   storeItem: StoreItem
+  applied: boolean
 }
 
 
 
 
-function StoreItemCard({ storeItem }: StoreItemCardProps) {
+function StoreItemCard({ storeItem, applied }: StoreItemCardProps) {
   return <div className="rounded-lg flex flex-col justify-between h-56 bg-cover bg-center overflow-hidden"
     style={{ background: `url('data:image/png;base64,${storeItem.image}')` }}>
-    <div className="flex items-center p-3 gap-1" >
-      <img className="w-6 h-6" src="/credit.png" />
-      <div className="text-[#ffd53c] text-lg">{storeItem.credit}</div>
-      <div className="flex-grow" />
-      <FaHeart className="w-6 h-6" />
+    <div className="flex p-3 items-start justify-between" >
+      <div className="flex bg-white rounded-full items-center gap-1 px-0.5 pe-1.5">
+        <img className="w-6 h-6" src="/credit.png" />
+        <div className="text-[#bf9706] text-lg font-bold">{storeItem.credit}</div>
+      </div>
+      {applied && <div className="bg-white rounded-full p-2">
+        <FaHeart className="w-6 h-6 text-primary" />
+      </div>}
     </div>
-    <div className="backdrop-blur backdrop-brightness-75 p-2">
+    <div className="backdrop-blur-2xl bg-background px-4 py-2">
       <div className="text-lg">{storeItem.title}</div>
-      <div className="text-muted-foreground overflow-ellipsis">{storeItem.description}</div>
+      <div className="opacity-80 overflow-ellipsis">{storeItem.description.slice(0, 90)}{storeItem.description.length > 90? "..." : ""}</div>
     </div>
   </div>
 
--- frontend/src/pages/store/ui/store.page.tsx
+++ frontend/src/pages/store/ui/store.page.tsx
@@ -1,12 +1,15 @@
 import { axios } from '@/core/axios';
+import { RootState } from '@/core/state';
 import { StoreItem } from '@/models/storeItem';
 import { Layout } from '@/packages/layout';
 import StoreItemCard from '@/packages/storeItems/store-item-card';
 import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 export function StorePage() {
     const [storeItems, setStoreItems] = useState<StoreItem[]>([])
+    const { id } = useSelector((state: RootState) => state.auth);
     useEffect(() => {
         axios.get<{ items: StoreItem[] }>("/api/storeItems").then((res) => {
             setStoreItems(res.data.items)
@@ -15,7 +18,7 @@ export function StorePage() {
     return (
         <Layout showNavigation backgroundImage="/background_green_pale.png">
             <div className="flex flex-col gap-4 p-4">
-                {storeItems.map(si => <Link key={si.id!} to={`${si.id!}`}><StoreItemCard storeItem={si} /></Link>)}
+                {storeItems.map(si => <Link key={si.id!} to={`${si.id!}`}><StoreItemCard storeItem={si} applied={!!si.redeemers?.find(r => r.redeemerUserId === id)} /></Link>)}
             </div>
         </Layout>
     );
